9ba8d23e006c0107a6597867fbdf45bf99757f48
UPDOT\soura <sourab@updot.in>
2023-02-08 21:08:21 +0530

    feat(model): created business schema

--- src/models/business.model.js
+++ src/models/business.model.js
@@ -1,2 +1,64 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+
+const BusinessSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "user is required"],
+    },
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    phone_number: {
+      type: String,
+      required: [true, "phone number is required"],
+    },
+    mobile_number: {
+      type: String,
+      required: [true, "mobile number is required"],
+    },
+    address: {
+      type: String,
+      require: [true, "address, is required"],
+    },
+    city: {
+      type: String,
+      required: [true, "city name is required"],
+    },
+    state: {
+      type: String,
+      required: [true, "state name is required"],
+    },
+    pincode: {
+      type: String,
+      required: [true, "pincode name is required"],
+    },
+    country: {
+      type: String,
+      required: [true, "country name is required"],
+    },
+    base_currency: {
+      type: String,
+    },
+    timezone: {
+      type: String,
+    },
+    fiscal_month: {
+      type: String,
+    },
+    fiscal_day: {
+      type: String,
+    },
+    date_format: {
+      type: String,
+    },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+const BusinessModal = mongoose.model("user", BusinessSchema);
+module.exports = BusinessModal;
